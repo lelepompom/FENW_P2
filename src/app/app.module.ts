111a54a9963bd96ec79148f1cd5e43e2fb25398a
Sandra Ortega <sandrita.saan@gmail.com>
2018-12-06 14:21:52 +0100

    Create app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { InstalacionesComponent } from './pages/instalaciones/instalaciones.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { ReservasComponent } from './pages/reservas/reservas.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InicioComponent,
+    InstalacionesComponent,
+    LoginComponent,
+    RegistroComponent,
+    ReservasComponent,
+    ServiciosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
